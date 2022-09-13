e0c7f210c3bd0a59b902e4713d3826b646738588
Bryan Somto <bryansomto@gmail.com>
2022-09-13 04:36:51 +0100

    begun task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,6 +38,16 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def delete(self, obj=None):
+        """Delete obj from __objects if it's inside
+        """
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+
+            if self.__objects[key]:
+                del self.__objects[key]
+                self.save()
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
