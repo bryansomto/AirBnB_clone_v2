4d881fdf22f2d8d15e7293e315c74744c36a3076
Bryan Somto <bryansomto@gmail.com>
2022-09-13 03:44:39 +0100

    added skipIf method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,6 +6,9 @@ from models import storage
 import os
 
 
+@unittest.skipIf(
+    os.getenv('HBNB_TYPE_STORAGE') == 'db',
+    "This test only work in Filestorage")
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
