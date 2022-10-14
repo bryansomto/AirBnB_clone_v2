2ddbbd804118dfc5df0567afc4e10b89156e7096
bryansomto <bryansomto@gmail.com>
2022-10-14 05:33:49 -0700

    modified Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,10 +4,13 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_hbnb():
     """ a function that returns the string 'Hello HBNB!'."""
+
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
