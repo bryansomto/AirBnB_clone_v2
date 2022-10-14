6b6ea68bf198bc581fc912c0b763bb1a50f86533
bryansomto <bryansomto@gmail.com>
2022-10-14 05:23:16 -0700

    modified Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ app.url_map.strict_slashes = False
 
 @app.route('/')
 def hello_hbnb():
-    """ a function that returns a string."""
+    """ a function that returns the string 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 if __name__ == "__main__":
