1f35b7c28045dd2496764626de4cb58c071b9670
bryansomto <bryansomto@gmail.com>
2022-10-14 17:18:16 -0700

    modified task 2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,27 +2,26 @@
 """ A script thats starts a Flask web application."""
 from flask import Flask
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ a function that returns the string 'Hello HBNB!'."""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ a function that returns the string 'HBNB'."""
-    return "HBNB"
+    return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_text(text):
+@app.route('/c/<text>', strict_slashes=False)
+def my_text(text):
     """a function that display 'C' followed by the value
     of the text variable."""
-    return "C %s" % escape(text.replace('_', ' '))
+    return 'C ' + str(text.replace('_', ' '))
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
