7b632ed5411ae96d20ed9c86bb4217c930e1d0e0
bryansomto <bryansomto@gmail.com>
2022-10-14 17:52:26 -0700

    fixed PEP8 requirement fail

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -18,7 +18,7 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """ 
+    """
     A function that display 'C' followed by the value
     of the text variable.
     """
